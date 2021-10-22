e6426be84fd581857ba59796550bc5d2acebb27e
120356aa <yurlovandrew@gmail.com>
2021-10-22 16:54:36 -0700

    dotenv

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
     app: {
         px: '!',
-        token: 'ODk2MjU3NTAwNjMyOTg5NzE3.YWEe1w.C0twXY9zDFc7yRnMEysDSBEwBEM',
+        token: process.env.DISCORD_TOKEN,
         playing: 'by Sn0w'
     },
 
--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const { Player } = require('discord-player');
 const { Client, Intents } = require('discord.js');
+require('dotenv').config()
 
 global.client = new Client({
     intents: [
