1ac725a651cd5d4efa92e422b9b58098595f9cd0
120356aa <yurlovandrew@gmail.com>
2021-10-23 19:55:55 -0700

    .

--- src/events.js
+++ src/events.js
@@ -23,6 +23,6 @@ player.on('channelEmpty', (queue) => {
     queue.metadata.send('Nobody is in the voice channel, leaving the voice channel... ❌');
 });
 
-player.on('queueEnd', setTimeout(() => (queue) => {
+player.on('queueEnd', (queue) => {
     queue.metadata.send('I finished reading the whole queue ✅');
-}), 600000);
+});
